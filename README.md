ec666972d7f606d47424639809c42f25202315c6
Han Lee <tmesis3@gmail.com>
2025-07-07 15:09:52 -0400

    esdros

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# render
+# Sprint_4_project-
+# Spring 4 Project 
+This project is aimed at developing a web application on a cloud service that is accessible to the public. It will have graphs and plots that are interactive and will visualize trends of a car advertisement dataset 
+
+Pandas was used to preprocess the data and prepare it for further analysis. While the Plotly Express library was used to plot histograms of different variables to reveal insights within the car advertisement dataset.
--- app.py
+++ app.py
@@ -1,53 +1,22 @@
-import pandas as pd
-import scipy.stats
 import streamlit as st
-import time
+import pandas as pd
+import plotly.express as px
+vehicles = pd.read_csv('./vehicles_us.csv')
+st.header("This is my header")
 
-# these are stateful variables which are preserved as Streamlit reruns this script
-if 'experiment_no' not in st.session_state:
-    st.session_state['experiment_no'] = 0
+#Plotly Express Histogram
+fig = px.histogram(vehicles, x='condition')
+fig.update_layout(title='Count of Cars by Condition', xaxis_title='Condition', yaxis_title='Count')
+st.plotly_chart(fig)
 
-if 'df_experiment_results' not in st.session_state:
-    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
+#Plotly Express scatterplot 
+fig_scatter = px.scatter(vehicles, x='model_year', y='price')
+fig_scatter.update_layout(title='Model Year vs Price', xaxis_title='Model Year', yaxis_title='Price')
 
-st.header('Tossing a Coin')
 
-chart = st.line_chart([0.5])
+#Checkbox 
+show_trend = st.checkbox("Show Price Trend")
+if show_trend:
+    fig_scatter.update_layout(title='Price Trend Over Time')
+st.plotly_chart(fig_scatter)
 
-def toss_coin(n):
-
-    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
-
-    mean = None
-    outcome_no = 0
-    outcome_1_count = 0
-
-    for r in trial_outcomes:
-        outcome_no +=1
-        if r == 1:
-            outcome_1_count += 1
-        mean = outcome_1_count / outcome_no
-        chart.add_rows([mean])
-        time.sleep(0.05)
-
-    return mean
-
-number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
-start_button = st.button('Run')
-
-if start_button:
-    st.write(f'Running the experiment of {number_of_trials} trials.')
-    st.session_state['experiment_no'] += 1
-    mean = toss_coin(number_of_trials)
-    st.session_state['df_experiment_results'] = pd.concat([
-        st.session_state['df_experiment_results'],
-        pd.DataFrame(data=[[st.session_state['experiment_no'],
-                            number_of_trials,
-                            mean]],
-                     columns=['no', 'iterations', 'mean'])
-        ],
-        axis=0)
-    st.session_state['df_experiment_results'] = \
-        st.session_state['df_experiment_results'].reset_index(drop=True)
-
-st.write(st.session_state['df_experiment_results'])
